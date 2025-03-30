6884ce18a7dce4c5a6669fccdcc9ac32e622bf81
Nehemiah Nesanathan <144255177+NrGjoe846@users.noreply.github.com>
2025-03-30 19:32:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Landing from "./pages/Landing";
 import Index from "./pages/Index";
 import Courses from "./pages/Courses";
@@ -22,6 +23,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <Navigation />
         <Routes>
           {/* Landing page only accessible directly */}
           <Route path="/" element={<Landing />} />
